356584b76bfe997bc775a9cf9d250dc405049c4d
llin0018 <56210135+llin0018@users.noreply.github.com>
2019-10-11 19:20:48 +1100

    as

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
     res.send('Hello ggggg');
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT, function () {
     var host = server.address().address;
     var port = server.address().port;
 
